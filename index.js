3673dbae10f733b4ec0f2fddd1f3bd58584cde7f
Matt He <matthlh123@gmail.com>
2022-05-18 20:05:43 -0400

    Able to retrieve data from Task Database on Notion Formatted and fixed a couple of bugs: Able to filter specific day, differentiate between page mention and text name

--- index.js
+++ index.js
@@ -4,27 +4,66 @@ const { Client } = require('@notionhq/client')
 const notion = new Client({ auth: process.env.NOTION_KEY })
 const databaseId = process.env.NOTION_DATABASE_ID
 
-async function addItem(text) {
-	try {
-		const response = await notion.pages.create({
-			parent: { database_id: databaseId },
-			properties: {
-				title: {
-					title: [
+async function getTasks() {
+	const response = await notion.databases.query({
+		database_id: databaseId,
+		filter: {
+			and: [
+				{
+					or: [
 						{
-							text: {
-								content: text,
+							property: 'Date TODO',
+							date: {
+								on_or_before: new Date(),
+							},
+						},
+						{
+							property: 'Date TODO',
+							date: {
+								is_empty: true, // TODO: Get current Date
 							},
 						},
 					],
 				},
+				{
+					property: 'Done?',
+					checkbox: {
+						equals: false,
+					},
+				},
+			],
+		},
+		sorts: [
+			{
+				property: 'Date TODO',
+				direction: 'ascending',
 			},
-		})
-		console.log(response)
-		console.log('Success! Entry added.')
-	} catch (error) {
-		console.error(error.body)
-	}
+			{
+				property: 'Priority',
+				direction: 'ascending',
+			},
+		],
+	})
+	const results = response.results
+
+	const tasks = results.map((page) => {
+		var taskName = ''
+		if (page.properties.Name.title[0].type === 'text') {
+			taskName = page.properties.Name.title[0].text.content
+		} else {
+			taskName = page.properties.Name.title[0].mention.page.id
+		}
+
+		return {
+			id: page.id,
+			title: taskName,
+		}
+	})
+
+	return tasks
 }
 
-addItem('test')
+;(async () => {
+	const nTasks = await getTasks()
+	console.log(nTasks)
+})()
